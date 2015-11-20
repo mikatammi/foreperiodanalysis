cb9580e96ebda8a2128fdb7fe964f3c56aa1404b
Mika Tammi <mikatammi@gmail.com>
2015-11-20 21:20:10 +0200

    Fixed style in test_analyze_app to be PEP8 and using unittest library now
    Signed-off-by: Mika Tammi <mikatammi@gmail.com>

--- tests/test_analyze_app.py
+++ tests/test_analyze_app.py
@@ -1,8 +1,10 @@
-import os
+import unittest
 
-# Compile and run code to test out for trivial mistakes
-def test_import_app():
-    app_filename = 'bin/fpa'
-    with open(app_filename) as f:
-        code = compile(f.read(), app_filename, 'exec')
-        exec(code)
+
+class TestAnalyzeApp(unittest.TestCase):
+    def test_import_app(self):
+        """Compile and import code of application"""
+        app_filename = 'bin/fpa'
+        with open(app_filename) as f:
+            code = compile(f.read(), app_filename, 'exec')
+            exec(code)
