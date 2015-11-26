63089f2c886b387df7eb1c0378eff1d5e2d6fb09
Mika Tammi <mikatammi@gmail.com>
2015-11-26 15:08:24 +0200

    Created foreperiodanalysis module
    Signed-off-by: Mika Tammi <mikatammi@gmail.com>

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ config = {
         'requires': ['nose',
                      'pep8'],
         'provides': ['foreperiodanalysis'],
+        'packages': ['foreperiodanalysis'],
         'scripts': ['bin/fpa']
 }
 
